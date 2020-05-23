1beca69dc36e28b6b53a559e59918619c57ea438
Sunmin Roh <smroh17@bu.edu>
2020-05-23 04:18:58 -0400

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         안녕하세요
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+         你好
+        </p>
+        <p>
+        こんにちは
+        </p>
       </header>
     </div>
   );
